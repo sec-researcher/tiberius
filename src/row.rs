8a9acb11e0ddce3b1fce37800ba890c851075e0f
Amir hossein Mahboubi <7353420+sec-researcher@users.noreply.github.com>
2022-03-30 12:40:28 +0430

    Update row.rs

--- src/row.rs
+++ src/row.rs
@@ -233,7 +233,7 @@ impl From<&TypeInfo> for ColumnType {
 #[derive(Debug)]
 pub struct Row {
     pub(crate) columns: Arc<Vec<Column>>,
-    pub(crate) data: TokenRow,
+    pub data: TokenRow,
     pub(crate) result_index: usize,
 }
 
